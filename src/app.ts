4ec66e43a8c2f5c776aa3579ffc0cdaa553f3d05
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2024-04-22 13:43:00 -0300

    estableciendo drpabble area

--- src/app.ts
+++ src/app.ts
@@ -190,7 +190,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
     // en vez de usar bind al final de this.dragStartHandler, se usa el decorator @autobind ya codeado
     @autobind
     dragStartHandler(event: DragEvent) {
-        console.log(event);
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent) {
@@ -224,12 +225,18 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
     }
 
     @autobind
-    dragOverHandler(_: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable');
+    dragOverHandler(event: DragEvent) {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable');
+        }
     }
 
-    dropHandler(_: DragEvent) { }
+    dropHandler(event: DragEvent) {
+        console.log(event.dataTransfer!.getData('text/plain'));
+        
+    }
 
     @autobind
     dragLeaveHandler(_: DragEvent) {
