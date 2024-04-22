6e8e59e4e0fb18a23011739181a51e5b7fef4e04
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2024-04-22 14:50:55 -0300

    implementacion del drop

--- dist/app.js
+++ dist/app.js
@@ -45,6 +45,16 @@ class ProjectState extends State {
     addProject(title, description, numOfPeople) {
         const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
         this.projects.push(newProject);
+        this.updateListeners();
+    }
+    moveProject(projectId, newStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+    updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         }
@@ -157,7 +167,8 @@ class ProjectList extends Component {
         }
     }
     dropHandler(event) {
-        console.log(event.dataTransfer.getData('text/plain'));
+        const prjId = event.dataTransfer.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
     dragLeaveHandler(_) {
         const listEl = this.element.querySelector('ul');
@@ -200,6 +211,12 @@ __decorate([
     __metadata("design:paramtypes", [DragEvent]),
     __metadata("design:returntype", void 0)
 ], ProjectList.prototype, "dragOverHandler", null);
+__decorate([
+    autobind,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [DragEvent]),
+    __metadata("design:returntype", void 0)
+], ProjectList.prototype, "dropHandler", null);
 __decorate([
     autobind,
     __metadata("design:type", Function),
